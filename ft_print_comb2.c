fd69798f1b15e31161939b15238c36b9c24e8c6f
Hiba <hibabelmoumene05@gmail.com>
2025-08-14 13:26:04 +0100

    Update ft_print_comb2.c

--- ft_print_comb2.c
+++ ft_print_comb2.c
@@ -5,51 +5,51 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hait-bel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/13 20:19:15 by hait-bel          #+#    #+#             */
-/*   Updated: 2025/08/14 10:04:52 by hait-bel         ###   ########.fr       */
+/*   Created: 2025/08/14 10:39:22 by hait-bel          #+#    #+#             */
+/*   Updated: 2025/08/14 12:50:36 by hait-bel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-	void ft_pr(char i, char j,char k, char l)
+void    ft_pr(char i, char j, char k, char l)
 {
-	write(1, &i, 1);
-	write(1, &j, 1);
-	write(1, " ",1);
-	write(1, &k, 1);
-	write(1, &l, 1);
-
+        write(1, &i, 1);
+        write(1, &j, 1);
+        write(1, " ", 1);
+        write(1, &k, 1);
+        write(1, &l, 1);
 }
-	void ft_print_comb2(void){
-		char	i;
-		char	j;
-		char	k;
-		char	l;
-		i = '0';
-		while( i <= '9'){
-			j = '0';
-			while(j <= '9'){
-				k = '0';
-				while(k <= '9'){
-					l = j +1;
-					while(l <= '9'){
-						ft_pr(i, j, k, l);
-						if (!(i =='9' && j =='8' && k =='9' && l =='9'))
-							write(1, ", ", 2);
-					 l++;
-					}
-				
-				k++;
-				}
-			j++;
-		       	}
-		i++;
-		}
-	
-	}
 
-int main()
-{ ft_print_comb2();
-return 0;
+void    ft_print_comb2(void)
+{
+        char    i;
+        char    j;
+        char    k;
+        char    l;
+
+        i = '0';
+        while( i <= '9'){
+                j = '0';
+                while(j <= '8'){
+                        k = i ;
+                        l = j +1;
+                        while(k <= '9') {
+                                while(l <= '9'){
+                                        ft_pr(i, j, k, l);
+                                        if (!(i == '9' && j == '8' && k == '9' && l == '9'))
+                                                write(1, ", ", 2);
+                                        l++;
+                                }
+                                k++;
+                                l='0';
+                        }
+                        j++;
+                }
+                i++;
+        }
+}
+
+int main(){
+ft_print_comb2();
 }
